fa2131cacf23eddb2637146b940e971702df85b9
Hashim Khan <hashim.khan@rokkan.com>
2017-10-05 17:39:52 -0400

    Updated Facets and Optimization Queries

--- QueryOptimizationsPredicates.sh
+++ QueryOptimizationsPredicates.sh
@@ -1,21 +1,40 @@
 # Predicates Used to Optimize and Filter results
 
+#p.limit: It defines  the number of results ; Default is 10 and -1 will return all results.
+fulltext=the
+type=cq:Page
+p.limit=-1
 
-Specify p.hits=full, in which case all properties will be included for each node:
+#p.hits=full: All properties will be included for each node.
+fulltext=the
+type=cq:Page
+p.hits=full
+p.limit=-1
 
-http://localhost:4502/bin/querybuilder.json?p.hits=full&property=jcr%3atitle&property.value=Triangle
+#p.hits=selective : It specifies the properties you want to get in p.properties, separated by a space.
+fulltext=the
+type=cq:Page
+p.hits=selective
+p.properties=jcr:path jcr:primaryType
+p.limit=-1
 
-Use p.hits=selective and specify the properties you want to get in p.properties, separated by a space:
+#p.nodedepth : This include the Child Nodes of the Search Results. p.nodedepth=n, where n is the number of levels you want the query to return. Note that, in order for a child node to be returned, it must be specified by the properties selector (p.hits=full).
+fulltext=the
+type=cq:Page
+p.hits=full
+p.nodedepth=2
+p.limit=100
 
-http://localhost:4502/bin/querybuilder.json?p.hits=selective&p.properties=sling%3aresourceType%20jcr%3aprimaryType&property=jcr%3atitle&property.value=Triangle
+#p.guessTotal: Using this parameter can significantly improve the performance of queries that return large result sets because Oak does not need to calculate the exact number of the result set. The purpose of p.guessTotal parameter is to return the appropriate number of results that can be shown by combining the minimum viable p.offset and p.limit values.
 
+fulltext=the
+type=cq:Page
+p.limit=-1
+p.guessTotal=true
 
-Another thing you can do is include child nodes in the QueryBuilder response. In order to do this you need to specify p.nodedepth=n, where n is the number of levels you want the query to return. Note that, in order for a child node to be returned, it must be specified by the properties selector (p.hits=full). Example:
-
-http://localhost:4502/bin/querybuilder.json?p.hits=full&p.nodedepth=5&property=jcr%3atitle&property.value=Triangle
-
-
-
-•	p.limit: Defines  the number of results to return for a given query. The default is 10 and -1 will return all results.
-•	p.offset: a 0-based value that defines where the search results start. Changing this parameter is used to go to the “next” page of search results.
-p.guessTotal: Using this parameter can significantly improve the performance of queries that return large result sets because Oak does not need to calculate the exact number of the result set. The disadvantage is that since 
+#p.offset=n : Sets the offset for the search results
+fulltext=a
+type=cq:Page
+p.limit=-1
+p.guessTotal=true
+p.offset=10
